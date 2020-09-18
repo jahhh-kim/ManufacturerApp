1d986833aa7868200137e4e9a8b5e526de02a51d
jahhh-kim <jaes9930@gmail.com>
2020-09-18 13:44:12 +0900

    ManufacturerApp 솔루션 생성 및 리포지토리 클래스까지 구현

---
+++ ManufacturerApp/Models/IManufacturerRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManufacturerApp.Models
+{
+    public interface IManufacturerRepository
+    {
+        Task<Manufacturer> AddManufacturer(Manufacturer manufacturer);
+        Task<List<Manufacturer>> GetManufacturers();
+        Task<Manufacturer> GetManufacturer(int id);
+        Task<Manufacturer> EditManufacturer(Manufacturer manufacturer);
+        Task DeleteManufacturer(int id);
+    }
+
+}
---
+++ ManufacturerApp/Models/ManufacturerRepository.cs
@@ -0,0 +1,56 @@
+﻿using ManufacturerApp.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManufacturerApp.Models
+{
+    public class ManufacturerRepository : IManufacturerRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ManufacturerRepository(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+        //입력
+        public async Task<Manufacturer> AddManufacturer(Manufacturer manufacturer)
+        {
+            _context.Manufacturers.Add(manufacturer);
+            await _context.SaveChangesAsync();
+            return manufacturer;
+        }
+
+        //출력
+        public async Task<List<Manufacturer>> GetManufacturers()
+        {
+            return await _context.Manufacturers.OrderBy(m => m.Id).ToListAsync();
+        }
+        //상세
+        public async Task<Manufacturer> GetManufacturer(int id)
+        {
+            return await _context.Manufacturers.Where(m => m.Id == id).SingleOrDefaultAsync();
+        }
+        //수정
+        public async Task<Manufacturer> EditManufacturer(Manufacturer manufacturer)
+        {
+            _context.Entry(manufacturer).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return manufacturer;
+        }
+        //삭제
+        public async Task DeleteManufacturer(int id)
+        {
+            var manufacturer = await _context.Manufacturers.Where(m => m.Id == id).SingleOrDefaultAsync();
+            if(manufacturer != null)
+            {
+             _context.Manufacturers.Remove(manufacturer);
+                await _context.SaveChangesAsync();
+            }
+        }
+    
+    }
+
+}
