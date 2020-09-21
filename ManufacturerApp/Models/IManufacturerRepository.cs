a8756345889f3c48e31eb22b2879be6002b19671
jahhh-kim <jaes9930@gmail.com>
2020-09-21 10:58:28 +0900

    완료

--- ManufacturerApp/Models/IManufacturerRepository.cs
+++ ManufacturerApp/Models/IManufacturerRepository.cs
@@ -6,11 +6,11 @@ namespace ManufacturerApp.Models
 {
     public interface IManufacturerRepository
     {
-        Task<Manufacturer> AddManufacturer(Manufacturer manufacturer);
-        Task<List<Manufacturer>> GetManufacturers();
-        Task<Manufacturer> GetManufacturer(int id);
-        Task<Manufacturer> EditManufacturer(Manufacturer manufacturer);
-        Task DeleteManufacturer(int id);
+        Task<Manufacturer> AddManufacturer(Manufacturer manufacturer);  // 입력
+        Task<List<Manufacturer>> GetManufacturers();                    // 출력
+        Task<Manufacturer> GetManufacturer(int id);                     // 상세
+        Task<Manufacturer> EditManufacturer(Manufacturer manufacturer); // 수정
+        Task DeleteManufacturer(int id);                                // 삭제
     }
 
 }
